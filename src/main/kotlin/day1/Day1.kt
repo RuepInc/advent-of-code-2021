3bd35215abd0d2948948d583673587c5193b8695
Thomas Rüping <Rueping@smartsquare.de>
2021-12-01 13:10:09 +0100

    Day1 minor refactoring

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -6,52 +6,50 @@ import java.io.File
 
 class Day1 {
     companion object {
-        fun part1(depth: List<Int>): Int {
-            return depth
+        fun part1(depth: List<Int>) =
+            depth
                 .zipWithNext()
-                .count { it.second.minus(it.first) > 0 }
-        }
+                .count { it.second > it.first }
 
-        fun part2(depth: List<Int>): Int {
-            return depth
+        fun part2(depth: List<Int>) =
+            depth
                 .windowed(3)
                 .map { it.sum() }
                 .zipWithNext()
-                .count { it.second.minus(it.first) > 0 }
+                .count { it.second > it.first }
+    }
+
+    class Day1Test {
+        @Test
+        fun testPart1() {
+            val depth =
+                File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day1/testinput.txt").readLines()
+                    .map { it.toInt() }
+            assertEquals(7, part1(depth))
         }
-    }
-}
 
-class Day1Test {
-    @Test
-    fun testPart1() {
-        val depth = File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day1/testinput.txt")
-            .readLines()
-            .map { it.toInt() }
-        assertEquals(7, Day1.part1(depth))
-    }
+        @Test
+        fun outPart1() {
+            val depth =
+                File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day1/puzzleinput.txt").readLines()
+                    .map { it.toInt() }
+            println(part1(depth))
+        }
 
-    @Test
-    fun outPart1() {
-        val depth = File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day1/puzzleinput.txt")
-            .readLines()
-            .map { it.toInt() }
-        println(Day1.part1(depth))
-    }
+        @Test
+        fun testPart2() {
+            val depth =
+                File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day1/testinput.txt").readLines()
+                    .map { it.toInt() }
+            assertEquals(5, part2(depth))
+        }
 
-    @Test
-    fun testPart2() {
-        val depth = File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day1/testinput.txt")
-            .readLines()
-            .map { it.toInt() }
-        assertEquals(5, Day1.part2(depth))
-    }
-
-    @Test
-    fun outPart2() {
-        val depth = File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day1/puzzleinput.txt")
-            .readLines()
-            .map { it.toInt() }
-        println(Day1.part2(depth))
+        @Test
+        fun outPart2() {
+            val depth =
+                File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day1/puzzleinput.txt").readLines()
+                    .map { it.toInt() }
+            println(part2(depth))
+        }
     }
 }
