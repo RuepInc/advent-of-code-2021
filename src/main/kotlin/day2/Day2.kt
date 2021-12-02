71e33f3d1dbae5afb0b8276c6834dad68bd70621
Thomas Rüping <Rueping@smartsquare.de>
2021-12-02 15:25:21 +0100

    Day2

---
+++ src/main/kotlin/day2/Day2.kt
@@ -0,0 +1,105 @@
+package day1
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day2 {
+    companion object {
+        fun part1(commands: List<String>): Int {
+            val pos = Position()
+            commands
+                .map { Pair(it.substringBefore(' ').trim(), it.substringAfter(' ').trim().toInt()) }
+                .forEach {
+                    when (it.first) {
+                        "forward" -> pos.forward(it.second)
+                        "up" -> pos.up(it.second)
+                        "down" -> pos.down(it.second)
+                    }
+                }
+            return pos.out()
+        }
+
+
+        fun part2(commands: List<String>): Int {
+            val pos = AimedPosition()
+            commands
+                .map { Pair(it.substringBefore(' ').trim(), it.substringAfter(' ').trim().toInt()) }
+                .forEach {
+                    when (it.first) {
+                        "forward" -> pos.forward(it.second)
+                        "up" -> pos.up(it.second)
+                        "down" -> pos.down(it.second)
+                    }
+                }
+            return pos.out()
+        }
+    }
+
+    class Day1Test {
+        @Test
+        fun testPart1() {
+            val commands =
+                File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day2/testinput.txt").readLines()
+            assertEquals(150, part1(commands))
+        }
+
+        @Test
+        fun outPart1() {
+            val commands =
+                File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day2/puzzleinput.txt").readLines()
+            println(part1(commands))
+        }
+
+        @Test
+        fun testPart2() {
+            val commands =
+                File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day2/testinput.txt").readLines()
+            assertEquals(900, part2(commands))
+        }
+
+        @Test
+        fun outPart2() {
+            val commands =
+                File("/home/thomas/dev/learning/adventOfCode2021/src/main/resources/day2/puzzleinput.txt").readLines()
+            println(part2(commands))
+        }
+    }
+}
+
+
+data class Position(
+    var x: Int = 0,
+    var y: Int = 0
+) {
+    fun forward(distance: Int) {
+        x += distance
+    }
+    fun down(distance: Int) {
+        y += distance
+    }
+    fun up(distance: Int) {
+        y -= distance
+    }
+    fun out() = x * y
+
+}
+
+data class AimedPosition(
+    var x: Int = 0,
+    var y: Int = 0,
+    var aim: Int = 0
+) {
+    fun forward(distance: Int) {
+        x += distance
+        y += aim * distance
+    }
+    fun down(distance: Int) {
+        aim += distance
+    }
+    fun up(distance: Int) {
+        aim -= distance
+    }
+    fun out() = x * y
+
+}
